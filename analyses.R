005efe2183750e70f645bc5b8bc920cd6853fb02
Marcos Costa Vieira <mvieira@EE-ML-011.local>
2024-10-17 09:34:22 -0500

    Bootstrap to get CI for difference in Spearman correlation coefficient for different predictors of H5 titers

--- analyses.R
+++ analyses.R
@@ -4,6 +4,9 @@ library(DescTools)
 table_s1_data <- read_csv("data-raw/table_s1_healthy.csv")
 table_s2_data <- read_csv("data-raw/table_s2_healthy_and_day0vaccinee.csv")
 
+nrep <- 1000 # Number of bootstrap replicates
+
+
 # Given the data, and for a single antigen, fits separate linear models predicting
 # log titers based on age, year of birth, group1 imprinting probability, and H1N1
 # imprinting probability
@@ -54,6 +57,68 @@ compute_spearman_correlations <- function(data){
     ungroup()
 }
 
+# Takes difference in Spearman correlation between predictors 1 and 2,#
+# tests their significance using bootstrapping
+run_bootstrap_correlation_test <- function(data, predictor1, predictor2, nrep){
+  
+  # Internal function: given table of Spearman correlation results, 
+  # retrieves comparison of interest, puts in wide format
+  get_pw_comparison <- function(spearman_corrs, predictor1, predictor2){
+    spearman_corrs %>%
+      filter(predictor %in% c(predictor1, predictor2)) %>%
+      select(antigen, predictor, coef) %>%
+      mutate(predictor = case_match(predictor,
+                                    predictor1 ~ 1,
+                                    predictor2 ~ 2 )) %>%
+      pivot_wider(names_from = predictor, values_from = coef, names_prefix = "cor_predictor_") %>%
+      # We care about the absolute value of the correlation coefficient
+      mutate(across(!matches("antigen"), abs)) %>%
+      select(antigen, cor_predictor_1, cor_predictor_2) %>%
+      mutate(cor_diff = cor_predictor_1 - cor_predictor_2)
+  }
+  
+  obs_corr_diff <- compute_spearman_correlations(data) %>%
+   get_pw_comparison(predictor1 = predictor1, predictor2 = predictor2) %>%
+    rename_with(.fn = function(x){paste0(x,"_obs")}, .cols = !matches("antigen"))
+  
+  bootstrap_corr_diff <- c()
+  
+  for(i in 1:nrep){
+    for(antigen in unique(data$antigen)){
+      
+      row_indices <- (1:nrow(data))[data$antigen == antigen]
+      
+      resampled_rows <- sample(row_indices, replace = T)
+      
+      resampled_data <- data[resampled_rows,]
+      
+      
+      resampled_corr <- compute_spearman_correlations(resampled_data) %>%
+        get_pw_comparison(predictor1 = predictor1, predictor2 = predictor2)
+      
+      bootstrap_corr_diff <- bootstrap_corr_diff %>%
+        bind_rows(resampled_corr)
+      
+    }
+
+  }
+  
+  
+  bootstrap_results <- bootstrap_corr_diff %>% 
+    left_join(obs_corr_diff) %>%
+    group_by(antigen, cor_predictor_1_obs, cor_predictor_2_obs, cor_diff_obs) %>%
+    summarise(bootstrap_diff_mean = mean(cor_diff),
+              bootstrap_diff_lower = quantile(cor_diff, 0.025),
+              bootstrap_diff_upper = quantile(cor_diff, 0.975)) %>%
+    mutate(predictor1 = predictor1, predictor2 = predictor2) %>%
+    select(antigen, predictor1, predictor2, everything())
+  
+  return(bootstrap_results)
+  
+}
+
+
+
 # For the blood bank data set, compute Spearman correlations between titers to
 # each antigen and probability of imprinting with group1 and probability of
 # imprinting with H1N1
@@ -87,6 +152,39 @@ model_comparison <- left_join(model_comparison,
 
 write_csv(model_comparison, "model_comparison.csv")
 
+# As a complement to linear models, run bootstrap tests
+
+# Is year of birth more strongly associated with titers than age is?
+bootstrap_yob_vs_age <- run_bootstrap_correlation_test(table_s2_data,
+                                                       predictor1 = "yob",
+                                                       predictor2 = "age",
+                                                       nrep = nrep)
+
+# Is group1 imprinting probability more strongly associated with titers than age is?
+bootstrap_group1_vs_age <- run_bootstrap_correlation_test(table_s2_data,
+                                                            predictor1 = "imp_group1",
+                                                            predictor2 = "age",
+                                                            nrep = nrep)
+# Does H1N1 imprinting beat age? 
+bootstrap_h1N1_vs_age <- run_bootstrap_correlation_test(table_s2_data,
+                                                        predictor1 = "imp_h1n1",
+                                                        predictor2 = "age",
+                                                        nrep = nrep)
+
+
+# Does group 1 imprinting beat YOB?
+bootstrap_group1_vs_yob <- run_bootstrap_correlation_test(table_s2_data,
+                                                          predictor1 = "imp_group1",
+                                                          predictor2 = "yob",
+                                                          nrep = nrep)
+
+bootstrap_results <- 
+  bootstrap_yob_vs_age %>%
+  bind_rows(bootstrap_group1_vs_age) %>%
+  bind_rows(bootstrap_h1N1_vs_age) %>%
+  bind_rows(bootstrap_group1_vs_yob)
+
+write_csv(bootstrap_results,  "bootstrap_results.csv")
 
 
 
